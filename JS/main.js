94cd15e3dcd2c444eb92ac6e4372eb253759c311
juniormelo13 <alvasmj@hotmail.com>
2021-06-15 23:25:39 -0300

    Começando no JS

---
+++ JS/main.js
@@ -0,0 +1,95 @@
+/*function clicou(){
+    document.getElementById("Agradecimento").innerHTML = "<b>Obrigado por clicar</b>"
+    //console.log(document.getElementById("Agradecimento"))
+}
+
+function redirecionar(){
+    window.open("https://globo.com")
+    //window.location.href = "https://esporteclubebahia.com.br"
+}
+
+function trocar() {
+    document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse"
+    //alert("trocar texto")
+}
+function voltar() {
+    document.getElementById("mousemove").innerHTML = "Passe o mouse aqui"
+}
+function load() {
+    alert("Página Carregada")
+}
+function funcaochange(elemento) {
+    console.log(elemento.value)
+}*/
+
+/*function validar_idade(idade){
+    var validar
+    if (idade>=18) {
+        validar=true
+    } else{
+        validar=false
+    }
+    return validar
+}*/
+
+//var idade = prompt("Qual a sua idade?")
+//console.log(validar_idade(idade))
+
+/*function soma(n1, n2) {
+    return n1 + n2
+}*/
+
+/*function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome)
+}*/
+
+//alert(setReplace("Vai Japão", "Japão", "Brasil"))
+
+//alert(soma(5, 10))
+
+/*var d = new Date
+alert(d.getMinutes())
+alert(d.getMonth()+1)
+alert(d.getHours())*/
+
+/*var count
+for(count=0; count<=5; count++) {
+    console.log(count)
+}*/
+
+/*var count = 50
+while (count <= 100) {
+    console.log(count)
+    count++
+}*/
+
+/*var idade = prompt("QUal a sua idade?")
+if (idade >= 18) {
+    alert("Maior de idade")
+} else {
+    alert("Menor de idade")
+}*/
+
+/*var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"banana", cor:"amarela"}]
+console.log(frutas)*/
+
+/*var fruta = {nome:"maça", cor:"vermelha"}
+console.log(fruta.nome)
+alert(fruta.cor)*/
+//lista.push("uva")
+//lista.pop()
+//console.log(lista.toString())
+//console.log(lista)
+//console.log(lista.join("-"))
+//alert(lista[1])
+
+//var nome = "Alvacy Junior"
+//var n1 = 30
+//var n2 = 10
+//var frase = "Japão é o melhor time do mundo"
+//console.log(nome)
+//console.log(n1 * n2)
+//console.log(frase.toLowerCase())
+//alert(frase.replace("Japão", "Brasil"))
+//alert(idade + idade2)
+//alert("Meu nome é " + nome + " e tenho " + idade + " anos")
